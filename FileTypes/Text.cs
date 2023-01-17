2e2057beb64750c8b1ed9162443ef449a08c95bb
yuyu <hcsd7b34@gmail.com>
2023-01-17 21:23:28 +0900

    Add: テキストからMarkdownへの変換

--- FileTypes/Text.cs
+++ FileTypes/Text.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+using FormatConverter.Functions;
 
 namespace FormatConverter.FileTypes
 {
@@ -11,5 +14,18 @@ namespace FormatConverter.FileTypes
         public override string Name { get; } = "テキスト";
         public override string Id { get; } = "Text";
         public override string[] Extensions { get; } = { "txt" };
+
+        [ConvertMethod]
+        public byte[] ToMarkdown(byte[] data)
+        {
+            string str = data.GetString();
+            str = new Regex(@"[\\`*_}\])#+\-=.!~|$]").Replace(str, "\\$0");
+            str = str.Replace("&", "&amp;");
+            str = str.Replace("<", "&lt;");
+            str = str.Replace(">", "&gt;");
+            str = new Regex("\r?\n").Replace(str, "  \n");
+            str = new Regex(@"^ ( {3,})", RegexOptions.Multiline).Replace(str, "&#32;$1");
+            return str.GetBytes();
+        }
     }
 }
