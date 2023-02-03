8576348a8767d29f1bd3ef48e17ea9f8f24b9bc2
yuyu <hcsd7b34@gmail.com>
2023-02-03 23:23:48 +0900

    Add: JPEG→各種画像ファイル

--- FileTypes/Jpeg.cs
+++ FileTypes/Jpeg.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing.Imaging;
+
+using FormatConverter.Functions.FileTypeGroups;
 
 namespace FormatConverter.FileTypes
 {
@@ -11,5 +14,29 @@ namespace FormatConverter.FileTypes
         public override string Name { get; } = "JPEG";
         public override string Id { get; } = "JPEG";
         public override string[] Extensions { get; } = { "jpg", "jpeg", "jpe", "jfif", "jfi", "jif" };
+
+        [ConvertMethod]
+        public byte[] ToPNG(byte[] data)
+        {
+            return Image.ChangeFormat(data, ImageFormat.Png);
+        }
+
+        [ConvertMethod]
+        public byte[] ToGIF(byte[] data)
+        {
+            return Image.ChangeFormat(data, ImageFormat.Gif);
+        }
+
+        [ConvertMethod]
+        public byte[] ToTIFF(byte[] data)
+        {
+            return Image.ChangeFormat(data, ImageFormat.Tiff);
+        }
+
+        [ConvertMethod]
+        public byte[] ToBMP(byte[] data)
+        {
+            return Image.ChangeFormat(data, ImageFormat.Bmp);
+        }
     }
 }
